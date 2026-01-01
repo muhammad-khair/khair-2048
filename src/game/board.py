2dc24b68997a8f4cbd369d9a6f812dea0b5a30d8
Khair <muhammad_khair@outlook.com>
2026-01-01 23:52:12 +0800

    Feature: Add static constructor and equality

--- src/game/board.py
+++ src/game/board.py
@@ -1,4 +1,9 @@
-from typing import List, Optional
+from __future__ import annotations
+
+import random
+from typing import Any, List, Optional
+
+from src.game.constants import GOAL_NUMBER, GRID_LENGTH, START_COUNT, START_NUMBER
 
 
 type Board = List[List[Optional[int]]]
@@ -12,6 +17,31 @@ class GameBoard:
         self.__board = board
         self.goal = goal
         self.__prop_numbers = prop_numbers
+    @staticmethod
+    def create_new(
+        grid_length: int = GRID_LENGTH,
+        goal_number: int = GOAL_NUMBER,
+        starting_count: int = START_COUNT,
+        starting_number: int = START_NUMBER,
+    ) -> GameBoard:
+        board: Board = [
+            [None for _ in range(grid_length)]
+            for _ in range(grid_length)
+        ]
+
+        rng = lambda: random.randint(0, grid_length - 1)
+        for _ in range(starting_count):
+            r, c = rng(), rng()
+            while board[r][c] == starting_number:
+                r, c = rng(), rng()
+            board[r][c] = starting_number
+
+        return GameBoard(
+            board=board,
+            goal=goal_number,
+            prop_numbers=[starting_number, starting_number * 2],
+        )
+
 
     def move_left(self) -> None:
         pass
@@ -24,6 +54,16 @@ class GameBoard:
 
     def move_down(self) -> None:
         pass
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, GameBoard):
+            return False
+        conditions = (
+            self.__board == other.__board,
+            self.goal == other.goal,
+            self.__prop_numbers == other.__prop_numbers,
+        )
+        return all(conditions)
 
     def __str__(self) -> str:
         rows = (
--- test/game/test_board.py
+++ test/game/test_board.py
@@ -1,5 +1,8 @@
 import unittest
 
+from unittest import mock
+from unittest.mock import MagicMock
+
 from src.game.board import GameBoard
 
 
@@ -27,6 +30,79 @@ class GameBoardTest(unittest.TestCase):
                 prop_numbers=[2, 4],
             )
         self.assertEqual(str(context.exception), "Board is empty")
+
+    def test_equality_returns_true(self):
+        board_one = GameBoard(
+            board=[
+                [2, None, None, None],
+                [None, None, 2, None],
+                [None, None, None, None],
+                [None, 2, None, None],
+            ],
+            goal=2048,
+            prop_numbers=[2, 4],
+        )
+        board_two = GameBoard(
+            board=[
+                [2, None, None, None],
+                [None, None, 2, None],
+                [None, None, None, None],
+                [None, 2, None, None],
+            ],
+            goal=2048,
+            prop_numbers=[2, 4],
+        )
+        self.assertEqual(board_one, board_two)
+
+    def test_equality_returns_false(self):
+        board_one = GameBoard(
+            board=[
+                [2, None, None, None],
+                [None, None, 2, None],
+                [None, None, None, None],
+                [None, 2, None, None],
+            ],
+            goal=2048,
+            prop_numbers=[2, 4],
+        )
+        board_two = GameBoard(
+            board=[
+                [None, None, 2, None],
+                [None, None, None, None],
+                [None, None, 4, None],
+                [2, None, None, None],
+            ],
+            goal=2048,
+            prop_numbers=[2, 4],
+        )
+        self.assertNotEqual(board_one, board_two)
+
+    @mock.patch('src.game.board.random.randint')
+    def test_static_constructor(self, mock_randint: MagicMock):
+        # coordinates for random number placement
+        mock_randint.side_effect = [
+            0, 0,
+            1, 2,
+            1, 2,  # to test if slot is already populated so logic will skip
+            3, 1,
+        ]
+        generated_board = GameBoard.create_new(
+            grid_length=4,
+            goal_number=2048,
+            starting_count=3,
+            starting_number=2,
+        )
+        expected_board = GameBoard(
+            board=[
+                [2, None, None, None],
+                [None, None, 2, None],
+                [None, None, None, None],
+                [None, 2, None, None],
+            ],
+            goal=2048,
+            prop_numbers=[2, 4],
+        )
+        self.assertEqual(generated_board, expected_board)
 
 
 if __name__ == "__main__":
